6de12db18ecd25fa7bf4507c9b0ca708f4631753
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-06-06 18:45:06 +0000

    Task 4

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print(''.join([chr(letter) for letter in range(ord('a'),
+print(''.join(['{}'.format(chr(letter)) for letter in range(ord('a'),
         ord('z')+1) if chr(letter) not in ['q', 'e']]), end='')
