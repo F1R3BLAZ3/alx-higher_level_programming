ed8ac74ea12b233004b8248328c56643513e672f
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-06-06 19:25:40 +0000

    Task 12

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def pow(a, b):
     result = a ** b
-    return int(result) if result.is_integer() else round(result, 2)
+    return int(result) if result == int(result) else result
