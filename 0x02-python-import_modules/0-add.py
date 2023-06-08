7baf8d6fcef571cb9875ff124e18199310413a47
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-06-08 18:48:58 +0000

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
+
 a = 1
 b = 2
-
-result = add_0.add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result))
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
