af3b8f18ab953de03930fe3ce2fdd2ead0df5675
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-06-08 18:35:28 +0000

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+a = 10
+b = 5
+calculator_1 = __import__('calculator_1')
+
+print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+print("{} - {} = {}".format(a, b, calculator_1.subtract(a, b)))
+print("{} * {} = {}".format(a, b, calculator_1.multiply(a, b)))
+print("{} / {} = {}".format(a, b, calculator_1.divide(a, b)))
