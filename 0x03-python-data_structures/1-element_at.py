c1a73ad0e62a912f68bdeb291433a7c712e5ae84
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-06-12 12:44:58 +0000

    Task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for i in my_list:
+        i = 0
+        my_list[i]
+        i += 1
+
+    for idx in my_list:
+        if (idx < 0):
+            break
+        elif (idx > i):
+            break
+        else:
+            return my_list[idx]
