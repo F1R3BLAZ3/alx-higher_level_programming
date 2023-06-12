d810028675ddcab6792cba557805eef33a83a1e3
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-06-12 13:11:37 +0000

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    i = 0
-    for element in my_list:
-        if (idx < 0):
-            return my_list
-        elif i == idx:
-            my_list.insert(idx, element)
-            return my_list
-        i += 1
-    return None
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
+
