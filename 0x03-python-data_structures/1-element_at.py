009f37615ee8c93eaa6c997021d846ce1514d2b2
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-06-12 12:48:49 +0000

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        i = 0
-        my_list[i]
-        i += 1
-
-    for idx in my_list:
+    i = 0
+    for element in my_list:
         if (idx < 0):
-            break
-        elif (idx > i):
-            break
-        else:
-            return my_list[idx]
+            return None
+        elif i == idx:
+            return element
+        i += 1
+    return None
