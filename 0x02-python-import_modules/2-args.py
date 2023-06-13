fd6c15bec62c2b239a72afe0d2cc024be003e2a4
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-06-13 19:50:38 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+import sys
+
+def main():
+    argv = sys.argv[1:]
+    num_args = len(argv)
+
+    if num_args == 0:
+        print("0 argument.")
+        print(".")
+    elif num_args == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[0]))
+    else:
+        print("{} arguments:".format(num_args))
+        for i, arg in enumerate(argv, 1):
+            print("{}: {}".format(i, arg))
+
+if __name__ == "__main__":
+    main()
