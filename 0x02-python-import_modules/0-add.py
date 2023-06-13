7d285c763eb1d59826d6bc79d9ddb9be65fc4b3d
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-06-13 13:11:55 +0000

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+def main():
     import add_0
-
     a = 1
     b = 2
     result = add_0.add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, result))
+
+if __name__ == "__main__":
+    main()
