58649e3995d39a678eff5251a74b649f23260d19
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-07-03 13:21:03 +0000

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,4 +7,39 @@ class Rectangle:
     """
     This class represents a rectangle.
     """
-    pass
+    def __init__(self, width=0, height=0):
+
+        self.__width = 0
+        self.__height = 0
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def height(self):
+
+        return self.__width
+
+    @width.setter
+    def height(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__size = value
