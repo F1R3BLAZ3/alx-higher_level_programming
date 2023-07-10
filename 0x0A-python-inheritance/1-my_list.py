98381ac8fe2fd084c0eb1d259ea4ce8619c2006a
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-07-10 21:37:57 +0000

    Task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+A custom list class that inherits from list
+"""
+
+
+class MyList(list):
+    """
+    A custom list class that inherits from list.
+
+    Public Methods:
+        print_sorted(self): Prints the list in sorted (ascending) order.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in sorted (ascending) order.
+
+        Returns:
+            None
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
