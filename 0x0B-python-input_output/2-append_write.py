1ec859f8738419f96f8523db7996f23072930cbf
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-07-11 21:30:34 +0000

    Task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
 
-def write_file(filename="", text=""):
+"""
+    Appends a string at the end of a text file (UTF8) and
+    returns the number of characters added.
+"""
+
+
+def append_write(filename="", text=""):
     """
-    Writes a string to a text file (UTF8) and returns the number of characters written.
+    Appends a string at the end of a text file (UTF8) and
+    returns the number of characters added.
 
     Args:
-        filename (str): The name of the file to write (default is an empty string).
-        text (str): The string to write to the file (default is an empty string).
+        filename (str): The name of the file to append to
+        (default is an empty string).
+        text (str): The string to append to the file
+        (default is an empty string).
 
     Returns:
-        int: The number of characters written to the file.
+        int: The number of characters added to the file.
     """
-    with open(filename, mode='w', encoding='utf-8') as file:
+    with open(filename, mode='a', encoding='utf-8') as file:
         return file.write(text)
 
+
 if __name__ == "__main__":
-    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-    print(nb_characters)
+    nb_characters_added = append_write(
+        "file_append.txt", "This School is so cool!\n")
+    print(nb_characters_added)
