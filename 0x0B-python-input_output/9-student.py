6c01c06e2b42af5d22119b8f85e0c2938a515cc2
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-07-11 23:27:26 +0000

    Task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+
+class Student:
+    """
+    Represents a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new instance of the Student class.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+
+    def __dict__(self):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        return self.to_json()
