0d4cdd3d07d388ff9324bcaee3790b1bd70569e8
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-16 19:29:46 +0000

    Task 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Inserts a new row into the table first_table.
-INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+-- Script to count the number of records with id = 89 in the first_table of the hbtn_0c_0 database
+SELECT COUNT(*) FROM first_table WHERE id = 89;
