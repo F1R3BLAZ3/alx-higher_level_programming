423eb7d39036f80d24fdac03e60c01ab13fb01a7
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-16 20:29:01 +0000

    Task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,14 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2.
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Script: 0-privileges.sql
+-- Task: List privileges of MySQL users user_0d_1 and user_0d_2 on localhost
+
+-- Connect to the MySQL server
+USE mysql;
+
+-- List privileges for user user_0d_1
+SELECT * FROM user WHERE user = 'user_0d_1';
+
+-- List privileges for user user_0d_2
+SELECT * FROM user WHERE user = 'user_0d_2';
+
+-- Disconnect from the MySQL server
+QUIT;
