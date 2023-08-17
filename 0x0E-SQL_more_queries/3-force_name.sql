6e1e2afc63a14088b406379f6b26c26ec56189a2
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-17 22:29:29 +0000

    Task 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,6 +2,6 @@
 
 -- If the table already exists, the following query will not fail due to the IF NOT EXISTS clause
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT,
     name VARCHAR(256) NOT NULL
 );
