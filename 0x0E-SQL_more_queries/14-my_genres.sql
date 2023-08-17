a8c5b20674cdee756b5b72ffa04138ef6dc564b4
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-17 22:43:40 +0000

    Task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,8 +2,11 @@
 -- It assumes the database name is provided as an argument to the mysql command.
 -- Query to retrieve genres of the show Dexter
 SELECT tv_genres.name
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_genres ON tv_show_genres.tv_genre_id = tv_genres.id
+  FROM tv_shows 
+       JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+
+       JOIN tv_genres
+       ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name ASC;
