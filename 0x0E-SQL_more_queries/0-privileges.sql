3f9fbcf8c070df93af815fa518475c1ebd9868e7
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-17 22:04:49 +0000

    Task 1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,12 +1,11 @@
--- Script to list privileges of MySQL users user_0d_1 and user_0d_2 on localhost
--- List privileges for user_0d_1
-SELECT * 
+-- Lists all privileges of the MySQL Users `user_0d_1`
+-- and `user_0d_2` in `localhost`.
+-- Show privileges for user_0d_1
+SELECT Grant_priv 
 FROM user
-WHERE user = 'user_0d_1'
-AND host = 'localhost';
+WHERE user = 'user_0d_1' AND host = 'localhost';
 
--- List privileges for user_0d_2
-SELECT * 
+-- Show privileges for user_0d_2
+SELECT Grant_priv 
 FROM user
-WHERE user = 'user_0d_2'
-AND host = 'localhost';
+WHERE user = 'user_0d_2' AND host = 'localhost';
