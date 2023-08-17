235ecd417d5b2dfc2980b91c44b0b85eac3619df
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-17 11:27:28 +0000

    Task 13

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,7 @@
+-- Lists all records with a score higher than 10
+-- of the table `second_table`
+-- of the database `hbtn_0c_0` in MySQL Server.
+DELETE
+    FROM second_table
+    WHERE score <= 5
+    *;
