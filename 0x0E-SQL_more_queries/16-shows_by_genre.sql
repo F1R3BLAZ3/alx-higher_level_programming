e1cfd5af39d2743ba3ac0ac670dfd8aca5a022b2
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-17 22:47:24 +0000

    Task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,16 +1,10 @@
 -- This script lists all TV shows and their linked genres from the hbtn_0d_tvshows database.
 
--- SQL query to list TV shows and their linked genres.
-SELECT
-    tv_shows.title AS show_title,
-    IFNULL(GROUP_CONCAT(tv_genres.name ORDER BY tv_genres.name ASC SEPARATOR ', '), 'NULL') AS genre_names
-FROM
-    tv_shows
-LEFT JOIN
-    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN
-    tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY
-    tv_shows.id
-ORDER BY
-    show_title ASC, genre_names ASC;
+SELECT tv_shows.title, tv_genres.name
+  FROM tv_shows
+       LEFT JOIN tv_show_genres
+       ON tv_shows.id = tv_show_genres.show_id
+
+       LEFT JOIN tv_genres
+       ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name ASC;
