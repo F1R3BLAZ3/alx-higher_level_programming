2dd56cd1fcdcacee4abcbca5fa03e57716d3d7fa
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-17 22:02:56 +0000

    Task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -2,8 +2,11 @@
 -- List privileges for user_0d_1
 SELECT * 
 FROM user
-WHERE user = 'user_0d_1';
+WHERE user = 'user_0d_1'
+AND host = 'localhost';
+
 -- List privileges for user_0d_2
 SELECT * 
 FROM user
-WHERE user = 'user_0d_2';
+WHERE user = 'user_0d_2'
+AND host = 'localhost';
