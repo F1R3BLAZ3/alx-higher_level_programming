d8673a0721faa96f89178e14d5e7c7a29cbace55
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-17 22:30:33 +0000

    Task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -4,9 +4,6 @@
 
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT NOT NULL DEFAULT 1,
-    name VARCHAR(256)
+    id INT DEFAULT 1,
+    name VARCHAR(256) NOT NULL
 );
-
--- Display a message indicating table creation status
-SELECT 'id_not_null table created' AS Message;
