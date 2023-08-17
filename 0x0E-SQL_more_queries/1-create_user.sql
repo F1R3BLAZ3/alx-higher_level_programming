23482e1c41244144c51b42a1e4f9641593ae92de
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-08-17 22:17:44 +0000

    Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,21 +1,3 @@
--- Script to create MySQL server user user_0d_1 with privileges
-
--- Check if the user user_0d_1 already exists
-SELECT 1 INTO @user_exists
-FROM mysql.user
-WHERE user = 'user_0d_1';
-
--- If the user doesn't exist, create the user
-SET @create_user_query = '';
-IF @user_exists IS NULL THEN
-    SET @create_user_query = 'CREATE USER \'user_0d_1\'@\'localhost\' IDENTIFIED BY \'user_0d_1_pwd\';';
-    PREPARE create_user_stmt FROM @create_user_query;
-    EXECUTE create_user_stmt;
-    DEALLOCATE PREPARE create_user_stmt;
-END IF;
-
--- Grant all privileges to user_0d_1
-SET @grant_privileges_query = 'GRANT ALL PRIVILEGES ON *.* TO \'user_0d_1\'@\'localhost\';';
-PREPARE grant_privileges_stmt FROM @grant_privileges_query;
-EXECUTE grant_privileges_stmt;
-DEALLOCATE PREPARE grant_privileges_stmt;
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
