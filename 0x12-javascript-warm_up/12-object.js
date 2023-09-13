ac3529988fcb402469522853d52df3e0698931a8
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-09-13 14:07:45 -0700

    task 12

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,30 +1,13 @@
 #!/usr/bin/node
 
-function findSecondLargest (numbers) {
-  if (numbers.length < 2) {
-    return 0;
-  }
+const myObject = {
+  type: 'object',
+  value: 12
+};
 
-  let largest = -Infinity;
-  let secondLargest = -Infinity;
+console.log(myObject);
 
-  for (let i = 0; i < numbers.length; i++) {
-    const num = parseInt(numbers[i]);
-    if (!isNaN(num)) {
-      if (num === 12) {
-        numbers[i] = 89; // Replace 12 with 89
-      }
-      if (num > largest) {
-        secondLargest = largest;
-        largest = num;
-      } else if (num > secondLargest && num < largest) {
-        secondLargest = num;
-      }
-    }
-  }
+// Update the value property
+myObject.value = 89;
 
-  return secondLargest;
-}
-
-const args = process.argv.slice(2);
-console.log(findSecondLargest(args));
+console.log(myObject);
