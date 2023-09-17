67eb9184bbd29a079f7d16d6b8b882eecb86f62e
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-09-17 20:57:10 +0000

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,47 +1,60 @@
 #!/usr/bin/python3
 
 """
-This script connects to a MySQL database, retrieves data from a 'states' table,
-and displays the results.
-"""
+    Fetch and print all state records from the database.
+    """
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Check if all 3 arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>"
-              .format(sys.argv[0]))
+
+def fetch_all_states():
+    """
+    Fetch and print all state records from the database.
+
+    Usage:
+    ./script.py <mysql_username> <mysql_password> <database_name>
+    """
+    try:
+        # Extract MySQL username, password, and database name
+        # from command line arguments
+        mysql_username = sys.argv[1]
+        mysql_password = sys.argv[2]
+        database_name = sys.argv[3]
+
+        # Connect to the MySQL server running on localhost at port 3306
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+        )
+
+        # Create a cursor object
+        cursor = db.cursor()
+
+        # Create the SQL query to retrieve all states sorted by states.id
+        sql_query = "SELECT * FROM states ORDER BY states.id ASC"
+
+        # Execute the SQL query
+        cursor.execute(sql_query)
+
+        # Fetch all rows as a list of tuples
+        states = cursor.fetchall()
+
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+
+        # Display the results
+        for state in states:
+            print(state)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
         sys.exit(1)
 
-    # Extract the arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
 
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
-
-    # Create a cursor object
-    cursor = db.cursor()
-
-    # Execute the SQL query to fetch states
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
-
-    # Fetch all rows as a list of tuples
-    states = cursor.fetchall()
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-
-    # Display the results
-    for state in states:
-        print(state)
+if __name__ == "__main__":
+    fetch_all_states()
