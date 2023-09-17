5baeb75eed5a2f126ea0633ddcbff00afdecffd7
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-09-17 20:37:09 +0000

    Task5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-
-"""
-    Query cities by state name from a MySQL database.
-    """
-
 import MySQLdb
 import sys
 
-
-def main():
-    """
-    Query cities by state name from a MySQL database.
-    """
+if __name__ == "__main__":
     # Check if all 4 arguments are provided
     if len(sys.argv) != 5:
         print("Usage: {} <mysql_username> <mysql_password> <database_name> <state_name>".format(sys.argv[0]))
@@ -38,31 +29,26 @@ def main():
 
         # Create the SQL query with user input (SQL injection free)
         sql_query = (
-            "SELECT cities.id, cities.name "
+            "SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') "
             "FROM cities "
-            "JOIN states ON cities.state_id = states.id "
-            "WHERE states.name = %s "
-            "ORDER BY cities.id"
+            "INNER JOIN states ON cities.state_id = states.id "
+            "WHERE states.name = %s"
         )
 
         # Execute the SQL query with the state name as a parameter
         cursor.execute(sql_query, (state_name,))
 
-        # Fetch all rows as a list of tuples
-        cities = cursor.fetchall()
+        # Fetch the result
+        result = cursor.fetchone()
 
         # Close the cursor and database connection
         cursor.close()
         db.close()
 
         # Display the results
-        for city in cities:
-            print(city)
+        if result and result[0]:
+            print(result[0])
 
     except MySQLdb.Error as e:
         print("MySQL Error: {}".format(e))
         sys.exit(1)
-
-
-if __name__ == "__main__":
-    main()
