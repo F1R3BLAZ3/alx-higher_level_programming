7e044448eca486ab706f7af20d98a92ef22e89c1
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-09-28 18:59:52 +0000

    Task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends an OPTIONS request to a URL using curl and displays the allowed HTTP methods
-curl -s -i -X OPTIONS "$1" | grep -i Allow | cut -d ":" -f 2- | tr -d '[:space:]'
+curl -s -X OPTIONS -i "$1" | grep -i Allow | cut -d ":" -f 2- | tr -d '[:space:]'
