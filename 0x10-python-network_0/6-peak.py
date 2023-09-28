033e825a5848fa26db6e9c1c2524792ab7dd6db9
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-09-28 19:52:40 +0000

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+    Find a peak element in a list of integers using a binary search algorithm.
+    """
+
 def find_peak(list_of_integers):
     """
     Find a peak element in a list of integers using a binary search algorithm.
