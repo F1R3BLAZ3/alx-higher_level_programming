1c2f060f8961f77f767584b51980b89b277d52a4
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-09-28 19:00:49 +0000

    Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to a URL using curl and displays the body of a 200 status code response
+# This script sends a GET request to a URL using curl and displays the response body for a 200 status code
 curl -s -o /dev/stdout -w "%{http_code}" "$1" | (grep -q 200 && cat /dev/stdin)
