3c6156df9c5abbbc7b9cf695b08219113f98024b
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-10-24 15:36:56 +0000

    Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,31 +2,26 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
+request(process.argv[2], (err, _res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const completedTasksByUsers = {};
+    body = JSON.parse(body);
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode === 200) {
-    const todos = JSON.parse(body);
+    for (let i = 0; i < body.length; ++i) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
 
-    // Create an object to store the count of completed tasks for each user
-    const completedTasksCount = {};
+      if (completed && !completedTasksByUsers[userId]) {
+        completedTasksByUsers[userId] = 0;
+      }
 
-    for (const todo of todos) {
-      if (todo.completed) {
-        // Increment the completed task count for the user
-        if (completedTasksCount[todo.userId]) {
-          completedTasksCount[todo.userId]++;
-        } else {
-          completedTasksCount[todo.userId] = 1;
-        }
+      if (completed) {
+        ++completedTasksByUsers[userId];
       }
     }
 
-    // Print the results in the expected format
-    console.log(JSON.stringify(completedTasksCount, null, 2));
-  } else {
-    console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
+    console.log(JSON.stringify(completedTasksByUsers, null, 2));
   }
 });
