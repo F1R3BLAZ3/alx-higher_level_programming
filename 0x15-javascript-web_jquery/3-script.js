83c9cca98d517fcd5483fd22c8216c723d82be69
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-05 14:51:15 +0000

    Task 3

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  // Wait for the DOM to be fully loaded
+  $('#red_header').click(function () {
+    $('header').addClass('red'); // Add the class "red" to <header>
+  });
+});
