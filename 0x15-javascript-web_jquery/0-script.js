485cbfef3910b8c4f3163606bfc62fb64dba4624
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-05 14:01:32 +0000

    Task 0

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Wait for the DOM to be fully loaded
+  const header = document.querySelector('header'); // Select the <header> element
+  header.style.color = '#FF0000'; // Set the text color to red
+});
