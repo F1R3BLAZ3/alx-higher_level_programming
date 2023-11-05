1ae31a1f66f0499d7c22a60c714c9d75dfb0372e
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-05 14:43:27 +0000

    Task 2

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  // Wait for the DOM to be fully loaded
+  $('#red_header').click(function () {
+    $('header').css('color', '#FF0000'); // Set the text color to red
+  });
+});
