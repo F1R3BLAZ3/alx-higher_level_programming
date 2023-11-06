9f22175b54dc1c06a2a47989eb25625d670fd3d7
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-06 19:06:14 +0000

    Task 6

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  // Wait for the DOM to be fully loaded
+  $('#update_header').click(function () {
+    // Update the text of the <header> element
+    $('header').text('New Header!!!');
+  });
+});
